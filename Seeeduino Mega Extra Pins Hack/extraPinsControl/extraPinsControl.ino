61c533ce91c29363f462d5bbb77ec0f2fa87271c
dontworryaboutit <searing.michael@gmail.com>
2014-07-21 14:21:42 -0400

    Added GNU GPL. Now we're legit

--- Seeeduino Mega Extra Pins Hack/extraPinsControl/extraPinsControl.ino
+++ Seeeduino Mega Extra Pins Hack/extraPinsControl/extraPinsControl.ino
@@ -1,5 +1,23 @@
 // make sure to select Tools > Board > Seeeduino Mega, or this code won't work
 
+/*
+breezySLAM_XBee.py - Seeeduino Mega code demonstrating extra pin usage
+
+Copyright (C) 2014 Michael Searing
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 unsigned long long nextBeat; // time in millis of next heart beat
 int beatDuration = 1000; // 1/2 heart beat period [ms]
