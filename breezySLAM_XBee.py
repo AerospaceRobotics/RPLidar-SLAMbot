b0600e698dc6e167fbd359db1716299f72796364
dontworryaboutit <searing.michael@gmail.com>
2014-07-25 17:08:14 -0400

    Updated to new version of BreezySLAM.  Added inset map.

--- breezySLAM_XBee.py
+++ breezySLAM_XBee.py
@@ -17,18 +17,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-print("Python {}".format('.'.join([str(el) for el in sys.version_info[0:3]])))
-
 GPL = "breezySLAM_XBEE.py Copyright (C) 2014 Michael Searing \n\
 This program comes with ABSOLUTELY NO WARRANTY; for details type [tbd]. \n\
 This is free software, and you are welcome to redistribute it \n\
 under certain conditions; type [tbd] for details."
 print(GPL)
 
-from breezyslam.algorithms import RMHC_SLAM
-from breezyslam.components import Laser
-from breezyslam.robots import WheeledRobot
+import sys
+print("Python {}.{}.{}".format(*sys.version_info[0:3]))
 
 if sys.version_info[0] < 3: # 2.x
   print("Getting Python 2 modules")
@@ -42,6 +38,9 @@ else: # 3.x
   import queue
   def raw_input(inStr): return input(inStr)
 
+from breezyslam.algorithms import RMHC_SLAM
+from breezyslam.components import Laser
+
 import time
 import threading # allow serial checking to happen on top of tkinter interface things
 
@@ -53,6 +52,7 @@ from scipy.ndimage.interpolation import rotate
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 # note that Data.saveImage() imports PIL and subprocess for map image saving and viewing
 
@@ -61,7 +61,7 @@ TALK_TO_XBEES = True if len(sys.argv) > 1 else False # non-blocking user input (
 # User preferences (default to True)
 USE_ODOMETRY = True
 INTERNAL_MAP = True
-LOG_ALL_DATA = True
+LOG_ALL_DATA = False
 
 # Macros (here's me sort of wishing this were C++...)
 def float2int(x):
@@ -86,8 +86,9 @@ MASK = 0b0000111111111111 # bits 0 .. 11
 NUM_SAMP = 360 # number of serial packets needed for 1 scan (guesstimate)
 
 # Map constants
-VIEW_SIZE_M = 4 # default size of region to be shown in display [m]
 MAP_SIZE_M = 16 # size of region to be mapped [m]
+VIEW_SIZE_M = 6 # default size of region to be shown in display [m]
+INSET_SIZE_M = 2 # size of relative map
 MAP_RES_PIX_PER_M = 100 # number of pixels of data per meter [pix/m]
 MAP_DEPTH = 10 # depth of data points on map (levels of certainty)
 
@@ -108,22 +109,23 @@ WHEEL_TRACK = 190.0 # separation of treads [mm]
 # Laser constants
 SCAN_SIZE = 360 # number of points per scan
 SCAN_RATE_HZ = 1980.0/360 # 1980points/sec * scan/360points [scans/sec]
-SCAN_ANGLE_MIN_DEG = ANG_MIN
-SCAN_ANGLE_MAX_DEG = ANG_MAX
+SCAN_DETECTION_ANGLE = ANG_MAX
 SCAN_DISTANCE_NO_DETECTION_MM = DIST_MAX
 SCAN_DETECTION_MARGIN = 0
-LASER_OFFSET_MM = -35
+LASER_OFFSET_MM = 35 # this value is negative what it should be # update() returns LIDAR unit position
 
 # BreezySLAM map constants
-MAP_SIZE_PIXELS = MAP_SIZE_M*MAP_RES_PIX_PER_M # number of pixels across the map
+MAP_SIZE_PIXELS = MAP_SIZE_M*MAP_RES_PIX_PER_M # number of pixels across the entire map
 MAP_SCALE_MM_PER_PIXEL = int(1000/MAP_RES_PIX_PER_M)
 MAP_QUALITY = 50
-HOLE_WIDTH_MM = 600
+HOLE_WIDTH_MM = 100
 RANDOM_SEED = 0xabcd
 
 # Map constants (derived)
-VIEW_SIZE_MM = 1000*VIEW_SIZE_M
-MAP_SIZE_MM = MAP_SIZE_M*1000 # size of data matrix [mm]
+MAP_SIZE_MM = int(MAP_SIZE_M*1000)
+VIEW_SIZE_MM = int(VIEW_SIZE_M*1000)
+INSET_SIZE_MM = int(INSET_SIZE_M*1000)
+INSET_SIZE_PIX = INSET_SIZE_M*MAP_RES_PIX_PER_M # number of pixels across the inset map
 MAP_CENTER_PIX = MAP_SIZE_PIXELS/2 # indices of map center []
 MM_2_PIX = MAP_RES_PIX_PER_M/1000.0 # [pix/mm]
 DEG_2_RAD = np.pi/180.0
@@ -140,8 +142,6 @@ ROT_2_DEG = 360.0 # [deg]
 REV_2_DEG = ROT_2_DEG*(REV_2_MM/ROT_2_MM) # degrees of rotation per wheel revolution [deg]
 DEG_2_TICK = REV_2_TICK/REV_2_DEG * ANGULAR_FLUX # [ticks/deg]
 TICK_2_DEG = REV_2_DEG/REV_2_TICK * 1.0/ANGULAR_FLUX # [deg/tick]
-
-print("Each pixel is " + str(round(1000.0/MAP_RES_PIX_PER_M,1)) + "mm, or " + str(round(1000.0/MAP_RES_PIX_PER_M/25.4,2)) + "in.")
 
 class Root(tk.Tk): # Tkinter window, inheriting from Tkinter module
   # init draws and displays window, creates data matrix, and calls updateData and updateMap
@@ -161,6 +161,8 @@ class Root(tk.Tk): # Tkinter window, inheriting from Tkinter module
     self.statusQueue = queue.Queue() # status of serial thread # FIFO queue by default
     self.RXQueue = queue.Queue() # data from serial to root # FIFO queue by default
     self.TXQueue = queue.Queue() # data from root to serial # FIFO queue by default
+    self.statusStr = tk.StringVar() # status of serThread
+    self.statusStr.set("Initializing")
     self.resetting = False
 
     # Start loops
@@ -168,23 +170,37 @@ class Root(tk.Tk): # Tkinter window, inheriting from Tkinter module
     self.serThread.start()
     self.initUI() # create all the pretty stuff in the Tkinter window
     self.restartAll(rootInit=True) # contains all initialization that needs to be re-done in case of a soft reset
+    print("Each pixel is " + str(round(1000.0/MAP_RES_PIX_PER_M,1)) + "mm, or " + str(round(1000.0/MAP_RES_PIX_PER_M/25.4,2)) + "in.")
     self.lift() # bring tk window to front if initialization finishes
 
   def initUI(self):
-    self.fig = plt.figure(figsize=(8, 5), dpi=131) # create matplotlib figure (calculated from $ xrandr)
-    self.ax = self.fig.add_subplot(111) # add plot to figure
-    self.ax.set_title("RPLIDAR Plotting") # name and label plot
-    self.ax.set_xlabel("X Position [mm]")
-    self.ax.set_ylabel("Y Position [mm]")
+    # current (and only) figure
+    self.fig = plt.figure(figsize=(9, 5), dpi=131) # create matplotlib figure (dpi calculated from $ xrandr)
+    gs = gridspec.GridSpec(1,3) # layout of plots in figure
 
+    # plot color settings
     cmap = plt.get_cmap("binary") # opposite of "gray"
     cmap.set_over("red") # robot is set to higher than MAP_DEPTH
+
+    # subplot 1 (stationary map)
+    self.ax1 = plt.subplot(gs[0,:2]) # add plot 1 to figure
+    self.ax1.set_title("Region Map") # name and label plot
+    self.ax1.set_xlabel("X Position [mm]")
+    self.ax1.set_ylabel("Y Position [mm]")
     dummyInitMat = np.zeros((2,2), dtype=np.uint8) # need to plot something to setup tkinter before dependent objects
-    self.myImg = self.ax.imshow(dummyInitMat, interpolation="none", cmap=cmap, vmin=0, vmax=MAP_DEPTH, # plot data
+    self.myImg1 = self.ax1.imshow(dummyInitMat, interpolation="none", cmap=cmap, vmin=0, vmax=MAP_DEPTH, # plot data
               extent=[-MAP_SIZE_MM/2, MAP_SIZE_MM/2, -MAP_SIZE_MM/2, MAP_SIZE_MM/2]) # extent sets labels by matching limits to edges of matrix
-    self.ax.set_xlim(-VIEW_SIZE_MM/2, VIEW_SIZE_MM/2) # pre-zoom image to defined default VIEW_SIZE_M
-    self.ax.set_ylim(-VIEW_SIZE_MM/2, VIEW_SIZE_MM/2)
-    self.cbar = self.fig.colorbar(self.myImg, orientation="vertical") # create colorbar
+    self.ax1.set_xlim(-VIEW_SIZE_MM/2, VIEW_SIZE_MM/2) # pre-zoom image to defined default VIEW_SIZE_MM
+    self.ax1.set_ylim(-VIEW_SIZE_MM/2, VIEW_SIZE_MM/2)
+
+    # colorbar
+    self.cbar = self.fig.colorbar(self.myImg1, orientation="vertical") # create colorbar
+
+    # subplot 2 (relative map)
+    self.ax2 = plt.subplot(gs[0,2]) # add plot 2 to figure
+    self.ax2.set_title("Robot Environs") # name and label plot
+    self.myImg2 = self.ax2.imshow(dummyInitMat, interpolation="none", cmap=cmap, vmin=0, vmax=MAP_DEPTH, # plot data
+              extent=[-INSET_SIZE_MM/2, INSET_SIZE_MM/2, -INSET_SIZE_MM/2, INSET_SIZE_MM/2])
 
     # turn figure data into matplotlib draggable canvas
     self.canvas = FigureCanvasTkAgg(self.fig, master=self) # tkinter interrupt function
@@ -207,7 +223,6 @@ class Root(tk.Tk): # Tkinter window, inheriting from Tkinter module
     self.entryBox = tk.Entry(master=self)
     self.entryBox.pack(side="left", padx = 5)
     tk.Button(self, text="Send (enter)", command=lambda: self.sendCommand(loop=False)).pack(side=tk.LEFT, padx=5) # tkinter interrupt function
-    self.statusStr = tk.StringVar() # status of serThread
     tk.Label(self, textvariable=self.statusStr).pack(side="left", padx=5, pady=5)
     tk.Button(self, text="Save Map", command=self.saveImage).pack(side=tk.LEFT, padx=5) # tkinter interrupt function
 
@@ -275,7 +290,8 @@ class Root(tk.Tk): # Tkinter window, inheriting from Tkinter module
     if loop and not self.resetting: self.after(CMD_RATE, lambda: self.sendCommand(resendCount=resendCount)) # tkinter interrupt function
 
   def updateData(self, init=False):
-    try:
+    self.dataInit = init
+    try: # do this before anything else
       status = self.statusQueue.get_nowait()
     except queue.Empty:
       pass
@@ -283,30 +299,31 @@ class Root(tk.Tk): # Tkinter window, inheriting from Tkinter module
       self.statusStr.set(status)
 
     if self.RXQueue.qsize() > NUM_SAMP: # ready to pull new scan data
-      self.getScanData() # pull data from serial thread via RXQueue
+      # pull data from serial thread via RXQueue
+      self.getScanData()
+
+      # update robot position
       if init: self.slam.prevEncPos = self.slam.currEncPos # set both values the first time through
-
-      self.data.robot_mm = self.slam.updateSlam(self.data.points) # send data to slam to do stuff
-      if init: self.data.robot_mm0 = self.data.robot_mm # set initial position during initialization
-
-      if INTERNAL_MAP:
-        dataMat = 255-np.flipud(np.resize(np.array(self.slam.breezyMap, dtype=np.uint8),(MAP_SIZE_PIXELS,MAP_SIZE_PIXELS)).T) # 7ms
-        self.data.matrix = (MAP_DEPTH/255.0*dataMat).astype(np.uint8) # 8ms
-      else:
-        self.data.drawPoints() # draw points, using updated slam, on the data matrix # 16ms
-      self.data.drawRobot() # new robot position
+      self.data.get_robot_pos(self.slam.updateSlam(self.data.points), init=init) # send data to slam to do stuff
 
       if init: init = False # initial data gathered successfully
 
     if not self.resetting: self.after(DATA_RATE, lambda: self.updateData(init=init)) # tkinter interrupt function
 
   def updateMap(self):
-    self.myImg.set_data(self.data.matrix) # 15ms
-    self.canvas.draw() # 400ms
+    if not self.dataInit: # wait until first data update to update map
+      self.data.drawMap(self.slam.breezyMap if INTERNAL_MAP else None) # draw points, using updated slam, on the data matrix # 16ms
+      self.data.drawInset() # new relative map
+      self.data.drawRobot(self.data.matrix, self.data.robot_pix) # new robot position
+
+      self.myImg1.set_data(self.data.matrix) # 15ms
+      self.myImg2.set_data(self.data.insetMatrix) # 15ms
+      self.canvas.draw() # 400ms
 
     if not self.resetting: self.after(MAP_RATE, lambda: self.updateMap()) # tkinter interrupt function
 
   def getScanData(self): # 50ms
+    self.data.points = [] # wipe old data before writing new data
     while True:
       try:
         queueItem = self.RXQueue.get_nowait()
@@ -330,15 +347,14 @@ class Slam(RMHC_SLAM):
   def __init__(self):
     laser = Laser(SCAN_SIZE, \
                   SCAN_RATE_HZ, \
-                  SCAN_ANGLE_MIN_DEG, \
-                  SCAN_ANGLE_MAX_DEG, \
+                  SCAN_DETECTION_ANGLE, \
                   SCAN_DISTANCE_NO_DETECTION_MM, \
                   SCAN_DETECTION_MARGIN, \
                   LASER_OFFSET_MM)
     RMHC_SLAM.__init__(self, \
                        laser, \
                        MAP_SIZE_PIXELS, \
-                       MAP_SCALE_MM_PER_PIXEL, \
+                       MAP_SIZE_M, \
                        MAP_QUALITY, \
                        HOLE_WIDTH_MM, \
                        RANDOM_SEED)
@@ -352,16 +368,17 @@ class Slam(RMHC_SLAM):
 
     for point in points: # create breezySLAM-compatible data from raw scan data
       dist = point[0]
-      index = int(point[1]) # index
-      if not 0 <= index <= SCAN_SIZE-1: continue
+      index = int(point[1])
+      if not 0 <= index < SCAN_SIZE: continue
       distVec[index] = int(dist)
 
     # note that breezySLAM switches the x- and y- axes (their x is forward, 0deg; y is right, +90deg)
     if LOG_ALL_DATA: dataFile.write(' '.join((str(el) for el in list(self.currEncPos)+distVec)) + '\n')
+    distVec = [distVec[i-180] for i in range(SCAN_SIZE)]
     self.update(distVec, self.getVelocities() if USE_ODOMETRY else None) # update slam information using particle filtering on LIDAR scan data // 10ms
     x, y, theta = self.getpos()
 
-    if INTERNAL_MAP: self.getmap(self.breezyMap)
+    if INTERNAL_MAP: self.getmap(self.breezyMap) # write internal map to breezyMap
 
     return (y, x, theta)
 
@@ -387,17 +404,19 @@ class Slam(RMHC_SLAM):
 class Data():
   # init creates data matrix and information vectors for processing
   # drawPoints adds scan data to data matrix
+  # drawInset adds scan data to inset matrix
   # drawRobot adds robot position to data matrix, generally using slam to find this position
   # saveImage uses PIL to write an image file from the data matrix
 
   def __init__(self):    
     self.matrix = np.zeros((MAP_SIZE_PIXELS, MAP_SIZE_PIXELS), dtype=np.uint8) # initialize data matrix
+    self.insetMatrix = np.zeros((INSET_SIZE_PIX, INSET_SIZE_PIX), dtype=np.uint8) # initialize inset matrix
     self.points = [] # current scan data
-    self.robot_mm0 = () # robot location data
-    self.robot_mm = () # x [mm], y [mm], th [deg], defined from lower-left corner of map
+    self.robot_init = () # x [mm], y [mm], th [deg], defined from lower-left corner of map
+    self.robot_rel = () # robot location relative to start position
+    self.robot_pix = () # robot pixel location, relative to upper-left (0,0) of image matrix
 
     self.robotSprite = np.array([[0,0,0,0,0,1,0,0,0,0,0], # shape of robot on map
-                                 [0,0,0,0,0,1,0,0,0,0,0],
                                  [0,0,0,0,0,1,0,0,0,0,0],
                                  [0,0,0,0,1,1,1,0,0,0,0],
                                  [0,0,0,0,1,1,1,0,0,0,0],
@@ -407,42 +426,48 @@ class Data():
                                  [0,0,1,1,0,0,0,1,1,0,0],
                                  [0,1,1,0,0,0,0,0,1,1,0],
                                  [0,1,1,1,1,1,1,1,1,1,0],
-                                 [1,1,1,1,1,1,1,1,1,1,1]])
+                                 [1,1,1,1,1,1,1,1,1,1,1],
+                                 [0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0]])
 
-  def drawPoints(self): # 7ms
-    xRobot_mm = (self.robot_mm[0] - self.robot_mm0[0]) # displacement from start position (center of map)
-    yRobot_mm = (self.robot_mm[1] - self.robot_mm0[1])
-    th_deg = self.robot_mm[2] - self.robot_mm0[2]
+  def get_robot_pos(self, curr_pos, init=False):
+    if init: self.robot_init = curr_pos
+    self.robot_rel = tuple([curr-init for (curr,init) in zip(curr_pos,self.robot_init)]) # displacement from start position (center of map)
+    xpix = float2int(curr_pos[0]*MM_2_PIX) # robot wrt map center (mO) + mO wrt matrix origin (xO)  = robot wrt xO [pix]
+    ypix = MAP_SIZE_PIXELS-float2int(curr_pos[1]*MM_2_PIX) # y is negative because pixels increase downwards (+y_mm = -y_pix = -np rows)
+    self.robot_pix = (xpix, ypix, self.robot_rel[2])
 
-    for point in self.points:
-      dist = point[0]
-      ang = point[1]
-      x_pix = float2int( MAP_CENTER_PIX + ( xRobot_mm + dist*np.sin((ang+th_deg)*DEG_2_RAD) )*MM_2_PIX ) # pixel location of scan point
-      y_pix = float2int( MAP_CENTER_PIX - ( yRobot_mm + dist*np.cos((ang+th_deg)*DEG_2_RAD) )*MM_2_PIX ) # point wrt robot + robot wrt xO
-      try:
-        self.matrix[y_pix, x_pix] += self.matrix[y_pix, x_pix] < MAP_DEPTH # increment value at location if below maximum value
-      except IndexError:
-        print("scan out of bounds")
+  def drawMap(self, breezyMap): # 7ms
+    if INTERNAL_MAP: # get data from internal BreezySLAM map
+      dataMat = 255-np.flipud(np.resize(np.array(breezyMap, dtype=np.uint8),(MAP_SIZE_PIXELS,MAP_SIZE_PIXELS)).T) # 7ms
+      self.matrix = (MAP_DEPTH/255.0*dataMat).astype(np.uint8) # 8ms
 
-  def drawRobot(self):
-    # robot wrt map center (mO) + mO wrt matrix origin (xO)  = robot wrt xO [pix]
-    # y is negative because np array rows increase downwards (+y_mm = -y_pix = -np rows)
-    xRobot_pix = float2int(MAP_CENTER_PIX + (self.robot_mm[0] - self.robot_mm0[0])*MM_2_PIX)
-    yRobot_pix = float2int(MAP_CENTER_PIX - (self.robot_mm[1] - self.robot_mm0[1])*MM_2_PIX)
-    th_deg = self.robot_mm[2] - self.robot_mm0[2]
+    else: # use scan data directly
+      for point in self.points:
+        dist = point[0]
+        ang = point[1]
+        # pixel location of scan point # point wrt robot + robot wrt x0 = point wrt x0
+        x_pix = float2int( MAP_CENTER_PIX + ( self.robot_rel[0] + dist*np.sin((ang+self.robot_rel[2])*DEG_2_RAD) )*MM_2_PIX )
+        y_pix = float2int( MAP_CENTER_PIX - ( self.robot_rel[1] + dist*np.cos((ang+self.robot_rel[2])*DEG_2_RAD) )*MM_2_PIX )
+        try:
+          self.matrix[y_pix, x_pix] += self.matrix[y_pix, x_pix] < MAP_DEPTH # increment value at location if below maximum value
+        except IndexError:
+          print("scan out of bounds")
 
-    robotMat = rotate(self.robotSprite, -th_deg, output=int)
-    rShape = robotMat.shape # rows, columns
+  def drawInset(self):
+    x, y = self.robot_pix[0:2] # indices of center of robot in main map
+    raw = int(INSET_SIZE_PIX*0.75) # half the size of the main map segment to capture
+    rad = INSET_SIZE_PIX/2 # half the size of the final segment
 
-    hgt = (rShape[0]-1)/2 # indices of center of robot
-    wid = (rShape[1]-1)/2
-    for x in range(-wid, wid+1): # relative to center of robot
-      for y in range(-hgt, hgt+1):
-        if robotMat[y+hgt, x+wid] == 1: # robot should exist here
-          try:
-            self.matrix[yRobot_pix+y, xRobot_pix+x] = ROBOT_DEPTH
-          except IndexError:
-            print("robot out of bounds")
+    mapChunk = self.matrix[y-raw:y+raw, x-raw:x+raw]
+    self.insetMatrix = rotate(mapChunk, self.robot_rel[2], output=np.uint8, reshape=False)[raw-rad:raw+rad, raw-rad:raw+rad]
+    self.drawRobot(self.insetMatrix, (INSET_SIZE_PIX/2,INSET_SIZE_PIX/2,0))
+
+  def drawRobot(self, mapObject, pos):
+    robotMat = rotate(self.robotSprite, -pos[2], output=np.uint8)
+    hgt = (robotMat.shape[0]-1)/2 # indices of center of robot
+    wid = (robotMat.shape[1]-1)/2
+    mapObject[pos[1]-hgt:pos[1]+hgt+1, pos[0]-wid:pos[0]+wid+1][robotMat.astype(bool)] = ROBOT_DEPTH
 
   def saveImage(self):
     from PIL import Image # don't have PIL? sorry (try pypng)
